cd992cb89fe789edfa1bc810ccbc86aa269b44ea
Viktor Svahn <svahnv@gmail.com>
2025-12-25 21:53:47 +0100

    added groups to db cli.py

--- src/cmp/db/cli.py
+++ src/cmp/db/cli.py
@@ -19,6 +19,8 @@ def register_subcommand(subparsers):
         description=description,
         formatter_class=argparse.RawTextHelpFormatter,
     )
+    rand_group = parser.add_argument_group('random sampling')
+
     parser.add_argument(
         'mode',
         help='database action',
@@ -29,22 +31,22 @@ def register_subcommand(subparsers):
         help='input filename',
     )
     parser.add_argument(
+        '-o',
+        dest='output',
+        default=None,
+        help='output filename',
+    )
+    rand_group.add_argument(
         '-n',
         dest='n_samples',
         type=str,
         default=None,
         help='number of samples to draw (a number or a fraction)',
     )
-    parser.add_argument(
-        '--seed', '-s',
+    rand_group.add_argument(
+        '--seed',
         help='numpy random seed (default None)',
         type=int,
         default=None,
-    )
-    parser.add_argument(
-        '-o',
-        dest='output',
-        default=None,
-        help='output filename',
     )
     parser.set_defaults(func=db_main)
