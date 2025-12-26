a609475d88a67c5a5c53a2dbe20e70dc59ea446a
Viktor Svahn <svahnv@gmail.com>
2025-12-26 12:28:30 +0100

    updated the run.sh test file.

--- test/run.sh
+++ test/run.sh
@@ -8,7 +8,7 @@ cmp mdeq nvt argon_start.xyz -n 100 --vscale 1.05 --temperature 300 --density 0.
 cmp mdeq npt argon_start.xyz -n 100 --temperature 300 --pressure 1.0 --taut 0.5 --taup 0.1
 
 cmp vscan -h
-cmp vscan a argon_start.xyz -n 10 --max 2
+cmp vscan a argon_start.xyz -n 10 --max 2 -i 0
 cmp vscan m ec_emc.xyz -n 10 --max 2
 
 cmp db -h
