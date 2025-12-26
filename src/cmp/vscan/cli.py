375c32632daff65e53ba4e8303d43f41d1b5833b
Viktor Svahn <svahnv@gmail.com>
2025-12-26 12:01:32 +0100

    vscan can now handle specific indices or all structures in a given input, and its cli.py has been updated

--- src/cmp/vscan/cli.py
+++ src/cmp/vscan/cli.py
@@ -6,63 +6,69 @@ from cmp.vscan.main import main as vscan_main
 
 
 description = """
-asdasdsdasd
+CLI for generating volume scans of ASE compliant atomic/molecular structures.
+The generated structures have no force and energy labels but the the header in 
+input xyz-files is preserved.
+
 """
-
-example = """dadsadasds
-"""
-
-epilog = """Run:
-> vscan 
-"""
-
-
-# 80-23=57 spaces wide
-
-
-input_help = """dasda
-"""
-
-header_help = """dasda
-"""
+#Omitted output (-o) will always result in a dry-run.
 
 
 def register_subcommand(subparsers):
     parser = subparsers.add_parser(
         'vscan',
-        help='pls',
+        help='CLI for creating volume-scans of periodic molecular structures.',
         description=description,
-        epilog=epilog,
         formatter_class=argparse.RawTextHelpFormatter,
     )
+    vscan_group = parser.add_argument_group('volume-scan')
     parser.add_argument(
         'mode',
-        help='atoms (a) or molecules (m)',
+        help='atomic or moleculecular centre of mass',
+        choices=['a','m'],
     )
     parser.add_argument(
         'input',
-        help=input_help,
+        help='input filename',
     )
     parser.add_argument(
+        '-o',
+        dest='output',
+        default=None,
+        help='output filename',
+    )
+    vscan_group.add_argument(
+        '-i',
+        metavar='INDEX',
+        dest='index',
+        default=':',
+        type=str,
+        required=False,
+        help='structure index',
+    )
+    vscan_group.add_argument(
         '--min',
+        metavar='MIN_SCALE',
         default=0.95,
         type=float,
         required=False,
+        help='minimum cell scale factor',
     )
-    parser.add_argument(
+    vscan_group.add_argument(
         '--max',
+        metavar='MAX_SCALE',
         default=1.05,
         type=float,
         required=False,
+        help='maximum cell scale factor',
     )
-    parser.add_argument(
-        '-n', '--num_points',
-        default=20,
+    vscan_group.add_argument(
+        '-n', 
+        metavar='NUM_POINTS',
+        dest='num_points',
+        default=10,
         type=int,
         required=False,
-    )
-    parser.add_argument(
-        '--output', '-o',
-        default=None,
+        help='number of cell volumes',
     )
     parser.set_defaults(func=vscan_main)
--- src/cmp/vscan/main.py
+++ src/cmp/vscan/main.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from ase.io import read,write
 
+from cmp.utils import show_small_banner
+
 
 def vscan(atoms,mode,min_scale,max_scale,npoints,output_name):
 	new_atoms = []
@@ -15,6 +17,7 @@ def vscan(atoms,mode,min_scale,max_scale,npoints,output_name):
 	for x in np.linspace(min_scale,max_scale,npoints):
 		ats = atoms.copy()
 		ats.info = atoms.info
+		ats.info['config_type'] = 'vscan'
 
 		if mode == 'a':
 			ats.set_cell(ats.cell*x, scale_atoms=True)
@@ -36,9 +39,31 @@ def vscan(atoms,mode,min_scale,max_scale,npoints,output_name):
 			ats.positions += com
 			ats.set_cell(ats.cell*x, scale_atoms=False)
 			new_atoms.append(ats)
+		print(f'Scale factor = {x:.2f}\tCell volume = {ats.get_volume():.3f} Å3')
 	
-		print(f'Scale = {x:.2f}\tvolume = {ats.get_volume():.3f} Å3')
+	return new_atoms
 
+
+
+def generate_volume_scans(atoms,mode,min_scale,max_scale,npoints,output_name):
+	new_atoms = []
+
+	if type(atoms) != list:
+		atoms = [atoms]
+
+	for i,a in enumerate(atoms):
+		if len(atoms) > 1:
+			print(f'\nScanning structure {i+1}')
+		scan = vscan(
+			atoms=a,
+			mode=mode,
+			min_scale=min_scale,
+			max_scale=max_scale,
+			npoints=npoints,
+			output_name=output_name,
+		)
+		new_atoms += scan
+	
 	if output_name is not None:
 		write(output_name,new_atoms)
 
@@ -46,13 +71,29 @@ def vscan(atoms,mode,min_scale,max_scale,npoints,output_name):
 def main(args):
 	args = vars(args)
 	
-	atoms = read(args['input'])
+	input_file = args['input']
+	index = args['index']
+	atoms = read(input_file, index)
 
-	vscan(
+	show_small_banner()
+	if index == ':':
+		pass
+		print(f'Generating a volume-scan of all structures in {input_file} using Chempanion.\n')
+	else:
+		idx = int(index)+1
+		if idx == 1:
+			suffix = 'st'
+		elif idx == 2:
+			suffix = 'nd'
+		elif idx > 2:
+			suffix = 'th'
+		print(f'Generating a volume-scan of the {idx}{suffix} structure in {input_file} using Chempanion.\n')
+
+	generate_volume_scans(
 		atoms,
 		mode=args['mode'],
-		min_scale = args['min'],
-		max_scale = args['max'],
-		npoints = args['num_points'],
+		min_scale=args['min'],
+		max_scale=args['max'],
+		npoints=args['num_points'],
 		output_name=args['output'],
 	)
